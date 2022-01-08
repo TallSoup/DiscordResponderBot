16f7cb46993207c714eee4389939315ce184b642
Andrew <andrewtclarkson@gmail.com>
2022-01-08 12:59:46 -0500

    Add Kanye quotes, die roller, random number chooser with user interaction.

--- main.py
+++ main.py
@@ -19,16 +19,43 @@ def dad_joke():
     return formatted_dad_joke
 
 
+def kanye():
+    """returns a formatted kanye quote"""
+    kanye_names = ['Pablo', 'Yeezy', 'Yeezus', 'Ye', 'Mr.West',
+                   'Kan The Louis Vuitton Don (Kanye - Better Than Yours)', 'The Don (Video)',
+                   'Martin Louis the King, Jr. (Video)', 'KanYeezy (Jay-Z - Lucifer)',
+                   'The LeBron of Rhyme (Kanye - DIAND)', 'K - Rock (Kanye - Drive Slow)',
+                   "Omari/'Mari (Kanye - Only One)", 'The Black Zac Efron (Kenny West on the Cleveland Show)',
+                   'Evel Kanyevel (Kanye - Touch the Sky video)', 'Swag King Cole (Kanye - Cold)']
+    response = requests.get("https://api.kanye.rest")
+    formatted_kanye_quote = f"{response.json()['quote']}  -{random.choice(kanye_names)}"
+    return formatted_kanye_quote
+
+
+def dice_roll():
+    """rolls a 6 sided die, returns result"""
+    roll = random.randint(1, 6)
+    result = f"I rolled a 6 sided die and it landed on {roll}."
+    return result
+
+
+def random_picker(number):
+    """returns a random selection between 1 and the number passed to it"""
+    random_number = random.randint(1, int(number))
+    return random_number
+
+
 # list of greetings in reply to hello function
 replies = ["Hi!", "Hello!", "How are you?", "Greetings!", "Good day to you", "Hallo!", "Hola!", "Bonjour", "Hey!"]
 
-
+# create connection to discord
 client = discord.Client()
 
 
 @client.event
 async def on_ready():
     print('logged in as {0.user}'.format(client))
+
 
 
 @client.event
@@ -52,6 +79,29 @@ async def on_message(message):
     if message.content.startswith("/bothelp"):
         await message.channel.send(
             "Use the following commands: 'hello' will respond with a random greeting, 'inspire' will respond with a random insirational quote, and 'dad joke' will tell you a random dad joke.")
+
+    # Kanye Quote
+    if message.content.startswith("kanye"):
+        await message.channel.send(kanye())
+
+    # Dice Roll
+    if message.content.startswith("roll"):
+        await message.channel.send(dice_roll())
+
+    # Random Number Chooser
+    if message.content.startswith("random"):
+        # channel = message.channel
+        await message.channel.send("I'll pick a number, what is the highest number you want?")
+        # wait for user response, then call function
+        msg = await client.wait_for('message')
+        try:
+            if int(msg.content) > 0:
+                result = random_picker(msg.content)
+                await message.channel.send(f"I've randomly picked a number between 1 and {msg.content} and it is **{result}**.")
+            else:
+                await message.channel.send(f"{msg.content} is not valid, try again with a number greater than 0.")
+        except ValueError:
+            await message.channel.send(f"{msg.content} is not valid, try again with a number greater than 0.")
 
 
 # In order for this to work, you'll need your bots token (keep this a secret, call with your preferred method, see readme for direction)
