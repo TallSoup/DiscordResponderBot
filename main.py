d33937efb86aeb004a7de9c612cb7930cecf1713
Andrew <andrewtclarkson@gmail.com>
2022-01-07 20:05:11 -0500

    Update comments, improve variable naming, learned how to clone a repo in order to push new code from a different computer, add requirements.txt, not sure why it has packages I didn't use?

--- main.py
+++ main.py
@@ -6,20 +6,21 @@ import requests
 
 def quote():
     """returns a formatted quote with author"""
-    quote = requests.get("https://zenquotes.io/api/random").json()
-    formatted_quote = f"{quote[0]['q']} -{quote[0]['a']}"
+    response = requests.get("https://zenquotes.io/api/random").json()
+    formatted_quote = f"{response[0]['q']} -{response[0]['a']}"
     return formatted_quote
+
 
 def dad_joke():
     """returns a formatted dad joke"""
     headers = {"Accept": "application/json"}
-    joke = requests.get("https://icanhazdadjoke.com/", headers=headers).json()
-    formatted_dad_joke = joke['joke']
+    response = requests.get("https://icanhazdadjoke.com/", headers=headers).json()
+    formatted_dad_joke = response['joke']
     return formatted_dad_joke
 
 
-# list of greetings in reply to hello
-replies = ["Hi!", "Hello!", "How are you?", "Greetings!", "Good day to you", "Hallo!"]
+# list of greetings in reply to hello function
+replies = ["Hi!", "Hello!", "How are you?", "Greetings!", "Good day to you", "Hallo!", "Hola!", "Bonjour", "Hey!"]
 
 
 client = discord.Client()
@@ -29,22 +30,29 @@ client = discord.Client()
 async def on_ready():
     print('logged in as {0.user}'.format(client))
 
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
+    # Greeting
     if message.content.startswith('hello'):
         await message.channel.send(random.choice(replies))
-    
+
+    # Inspiration
     if message.content.startswith("inspire"):
         await message.channel.send(quote())
 
+    # Dad Joke
     if message.content.startswith("dad joke"):
         await message.channel.send(dad_joke())
-    
+
+    # Help Function
     if message.content.startswith("/bothelp"):
-        await message.channel.send("Use the following commands: 'hello' will respond with a random greeting, 'inspire' will respond with a random insirational quote, and 'dad joke' will tell you a random dad joke.")
+        await message.channel.send(
+            "Use the following commands: 'hello' will respond with a random greeting, 'inspire' will respond with a random insirational quote, and 'dad joke' will tell you a random dad joke.")
+
 
 # In order for this to work, you'll need your bots token (keep this a secret, call with your preferred method, see readme for direction)
 client.run(os.environ['TOKEN'])
